6ac7f38a676d842e0f01cba569f57ce89cdd03ca
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-30 03:28:56 -0500

    Update dashboard.lua

--- lua/custom/configs/dashboard.lua
+++ lua/custom/configs/dashboard.lua
@@ -1,29 +1,24 @@
--- ~/.config/nvim/lua/custom/dashboard.lua
+nvdash = {
+    load_on_startup = true,
 
-local db = require('dashboard')
+    header = {
+      "           ▄ ▄                   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
 
--- Set a custom header
-db.custom_header = {
-  'Welcome to My Neovim Setup!',
-  'Enjoy your coding session!',
-}
-
--- Set custom center menu
-db.custom_center = {
-  { icon = '  ',
-    desc = 'Recently opened files                   ',
-    action = 'Telescope oldfiles',
-    shortcut = 'SPC f r' },
-  { icon = '  ',
-    desc = 'Find File                               ',
-    action = 'Telescope find_files',
-    shortcut = 'SPC f f' },
-  { icon = '  ',
-    desc = 'File Browser                            ',
-    action = 'Telescope file_browser',
-    shortcut = 'SPC f b' },
-  { icon = '  ',
-    desc = 'Find word                               ',
-    action = 'Telescope live_grep',
-    shortcut = 'SPC f w' },
-}
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
