816678f211d286b02e06b378bc30a4b591e4fa83
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-12-30 03:37:28 -0500

    Update plugins.lua with nvdash

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,34 +2,34 @@ local plugins = {
   -- Install through mason
   {
     "williamboman/mason.nvim",
-  opts = {
+    opts = {
       ensure_installed = {
-      "autopep8", "biome", "black", "emmet-ls", "eslint-lsp", "eslint_d", "flake8", "isort", "jedi-language-server", "js-debug-adapter", "mypy", "prettier", "prettierd", "pyflakes", "pylint", "pyright", "python-lsp-server", "typescript-language-server", "yapf", "rust-analyzer", "tailwindcss-language-server", "html-lsp", "css-lsp", "lua-language-server"
+        "autopep8", "biome", "black", "emmet-ls", "eslint-lsp", "eslint_d", "flake8", "isort", "jedi-language-server", "js-debug-adapter", "mypy", "prettier", "prettierd", "pyflakes", "pylint", "pyright", "python-lsp-server", "typescript-language-server", "yapf", "rust-analyzer", "tailwindcss-language-server", "html-lsp", "css-lsp", "lua-language-server"
       }
     }
   },
   "roobert/tailwindcss-colorizer-cmp.nvim",
-  --lspconfig points to configs.lspconfig
+  -- lspconfig points to configs.lspconfig
   {
-  "neovim/nvim-lspconfig",
-    config = function ()
+    "neovim/nvim-lspconfig",
+    config = function()
       require("plugins.configs.lspconfig")
       require("custom.configs.lspconfig")
     end
   },
-  --rust auto format on save
+  -- rust auto format on save
   {
-  "rust-lang/rust.vim",
+    "rust-lang/rust.vim",
     ft = "rust",
     init = function()
       vim.g.rustfmt_autosave = 1
     end
   },
-  --working w/ crate toml files
+  -- working w/ crate toml files
   {
     "saecki/crates.nvim",
-    ft = {"rust", "toml"},
-    config = function ()
+    ft = { "rust", "toml" },
+    config = function()
       local crates = require("crates")
       crates.setup(opts)
       crates.show()
@@ -38,30 +38,59 @@ local plugins = {
   -- supports above 
   {
     "hrsh7th/nvim-cmp",
-    opts = function ()
+    opts = function()
       local M = require "plugins.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
+      table.insert(M.sources, { name = "crates" })
       return M
     end
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    config = function ()
+    config = function()
       require("plugins.configs.treesitter")
       require("custom.configs.treesitter")
     end
   },
-  --Vue lsp and syntax
-   { "leafOfTree/vim-vue-plugin" }, -- Vue filetype plugin
+  -- Vue lsp and syntax
+  { "leafOfTree/vim-vue-plugin" }, -- Vue filetype plugin
   { "L3MON4D3/LuaSnip" }, -- Snippets
   { "rafamadriz/friendly-snippets" }, -- Collection of snippets
 
-   -- TypeScript/React LSP and Syntax Highlighting
+  -- TypeScript/React LSP and Syntax Highlighting
   { "jose-elias-alvarez/typescript.nvim" }, -- TypeScript LSP
   { "maxmellon/vim-jsx-pretty" }, -- JSX syntax highlighting
 
   -- Optional: Prettier and ESLint
   { "jose-elias-alvarez/null-ls.nvim" }, -- Linter and formatter
+
+  -- Add nvdash plugin here
+  {
+    "NvChad/nvdash",
+    config = function()
+      require("nvdash").setup({
+        load_on_startup = true,
+        header = {
+          "           ▄ ▄                   ",
+          "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+          "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+          "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+          "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+          "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+          "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+          "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+          "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+        },
+        buttons = {
+          { "  Find File", "Spc f f", "Telescope find_files" },
+          { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+          { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+          { "  Bookmarks", "Spc m a", "Telescope marks" },
+          { "  Themes", "Spc t h", "Telescope themes" },
+          { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+      })
+    end
+  }
 }
 
 return plugins
