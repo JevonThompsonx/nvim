445bd7823c14cdeb5daf7599947b1fb2b0d2cd02
Jevon Thompson <JThompsonx00@gmail.com>
2025-06-16 09:26:14 -0700

    comment nvim changes

--- lua/custom/configs/treesitter.lua
+++ lua/custom/configs/treesitter.lua
@@ -1,7 +1,13 @@
 local options = {
-  ensure_installed = {"rust", "python", "javascript", "typescript", "css", "html", "vue",
-}
+  ensure_installed = { "rust", "python", "javascript", "typescript", "css", "html", "vue", "tsx" },
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
 }
 
 return options
-
